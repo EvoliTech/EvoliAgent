b61dd04bb4a630e243bc691a3326b3cd06b57693
Everton Moreno <open.evertonai@gmail.com>
2026-05-19 16:50:43 -0300

    fix: remove flex layout from main viewport wrapper to stop horizontal shifting of centered child pages

--- App.tsx
+++ App.tsx
@@ -185,7 +185,7 @@ export default function App() {
         subUserRole={subUserRole || 'admin'}
         userEmail={session?.user.email || ''}
       />
-      <div className="flex-1 overflow-auto bg-gray-50 relative flex flex-col">
+      <div className="flex-1 overflow-auto bg-gray-50 relative">
         {currentPage === 'dashboard' && <Dashboard onNavigate={navigateTo} />}
         {currentPage === 'agenda' && <Agenda />}
         {currentPage === 'appointments' && <AppointmentsList />}
